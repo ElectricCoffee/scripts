a15f080288a4364ac031c486595f61fc4b07b582
Niko Lepka <slench102+git@gmail.com>
2019-04-16 23:52:06 +0200

    change prompt to keyword arg

--- prompt.rb
+++ prompt.rb
@@ -2,7 +2,7 @@
 # Convenience function to deal with user prompts
 # Prints +text+ to the terminal, then passes the user's response into the block
 # +src+ is set to STDIN by default, but can be overridden if need be.
-def prompt(text, src = STDIN)
+def prompt(text, src: STDIN)
     print text
     input = src.gets.chomp
     yield input
